42401eaa74fbe3c1486d3c17bd263be64b99cb15
Andre Pires <erdnapfaserip@gmail.com>
2025-10-28 14:26:00 -0300

    feat: done functional-tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -18,6 +18,54 @@ suite('Functional Tests', function() {
                 assert.equal(res.body.returnUnit, 'gal');
                 assert.equal(res.body.string, '10 liters converts to 2.64172 gallons');
                 done();
-            })
+            });
     });
+    test('Convert an invalid input such as 32g: GET request to /api/convert', function(done) {
+        chai.request(server)
+            .get('/api/convert')
+            .query({ input: '32g' })
+            .end(function(err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'invalid unit');
+                done();
+            });
+    });
+    test('Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert', function(done) {
+        chai.request(server)
+            .get('/api/convert')
+            .query({ input: '3/7.2/4kg' })
+            .end(function(err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'invalid number');
+                done();
+            });
+    });
+    test('Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert', function(done) {
+        chai.request(server)
+            .get('/api/convert')
+            .query({ input: '3/7.2/4kilomegagram' })
+            .end(function(err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'invalid number and unit');
+                done();
+            });
+    });
+    test('Convert with no number such as kg: GET request to /api/convert', function(done) {
+        chai.request(server)
+            .get('/api/convert')
+            .query({ input: 'kg' })
+            .end(function(err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.initNum, 1);
+                assert.equal(res.body.initUnit, 'kg');
+                assert.approximately(res.body.returnNum, 2.20462, 0.1);
+                assert.equal(res.body.returnUnit, 'lbs');
+                assert.equal(res.body.string, '1 kilograms converts to 2.20462 pounds');
+                done();
+            });
+    });
+
+
+
+
 });
