f54674383afc56e6591c6d95f94c493fcf8ff7b7
Andre Pires <erdnapfaserip@gmail.com>
2025-10-28 11:49:17 -0300

    feat: start functional-tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,18 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+    test('Convert a valid input such as 10L: GET request to /api/convert', function(done) {
+        chai.request(server)
+            .get('/api/convert')
+            .query({ input: '10L' })
+            .end(function(err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.initNum, 10);
+                assert.equal(res.body.initUnit, 'L');
+                assert.approximately(res.body.returnNum, 2.64172, 0.1);
+                assert.equal(res.body.returnUnit, 'gal');
+                assert.equal(res.body.string, '10 liters converts to 2.64172 gallons');
+                done();
+            })
+    });
 });
